46d03c959e0aafdfd7b4d5a4f641aba69a13c691
alinazir105 <alinazir105@gmail.com>
2024-10-13 13:57:38 +0500

    herosection added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        backgroundImage: {
+          'hero': "url('./src/assets/hero-bg.jpg')",
+        }
+    },
   },
   plugins: [],
 }
