d8d2210f10c5e7504a07144ef54a458e9e8b6bd4
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-10-13 14:16:17 +0500

    Overview Section Added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../components/HomePageComponents/heroSection'
+import OverView from '../components/HomePageComponents/overView'
 import TeamSection from '../components/HomePageComponents/teamSection'
 const Home = () => {
   return (
     <div>
         <HeroSection/>
+        <OverView/>
         <TeamSection/>
     </div>
   )
